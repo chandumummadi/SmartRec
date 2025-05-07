a2ae0c7fcb84b4cec70914f9d534d28d2001cad3
Sharath Chandra Mummadi <80935059+chandumummadi@users.noreply.github.com>
2025-05-07 08:50:33 -0700

    Updated requirements

--- Smartrec-Backend/Backend/news/urls.py
+++ Smartrec-Backend/Backend/news/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('categories/', views.get_categories_articles, name='categories_articles'),
-    path('populate/', views.populate_news_data, name='populate_news_data'),
+    path('categories/', views.get_categories_articles, name='categories_articles'), #5
+    path('populate/', views.populate_news_data, name='populate_news_data'), #1
     path('recommend_news/', views.recommend_news, name='recommend_news'),
-    path('update_user_preferences/', views.update_user_preferences, name='update_user_preferences'),
-    path('user_preferences/', views.get_user_preferences_view, name = 'get_user_preferences'),
-    path('trending/', views.get_trending_news, name = 'get_trending_news'),
+    path('update_user_preferences/', views.update_user_preferences, name='update_user_preferences'), #2
+    path('user_preferences/', views.get_user_preferences_view, name = 'get_user_preferences'), #3
+    path('trending/', views.get_trending_news, name = 'get_trending_news'), #4
     path('handle_click/', views.handle_click_view, name = 'handle_user_click')
 ]
